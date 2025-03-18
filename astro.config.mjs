f2ad2e8f5b644708c7187937ad1052f09b50157b
Sam <sam@vividconcept.ai>
2025-03-18 10:55:20 -0700

    adding codestars, update astro and gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sam.land',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(), 
+		sitemap(), 
+		partytown({
+			config: {
+				forward: ["dataLayer.push", "gtag"],
+			},
+		}),
+	],
 });
